42094de856d1caf4c72cf8c516fb920ab9c45954
Chanu <kollichanu@gmail.com>
2026-03-14 20:42:41 -0500

    added argocd installation using helm

---
+++ helm.tf
@@ -0,0 +1,21 @@
+provider "helm" {
+  kubernetes = {
+	config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argocd" {
+
+  depends_on = [null_resource.update-kubeconfig]
+
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  set = [
+	{
+	  name  = "server.service.type"
+	  value = "LoadBalancer"
+	}
+  ]
+}
