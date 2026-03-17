463d40da1cbf51a724466580f9b1ffe5e468e971
Chanu <kollichanu@gmail.com>
2026-03-16 21:41:09 -0500

    Roboshop Project

--- helm.tf
+++ helm.tf
@@ -27,6 +27,7 @@ resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
+  values     = [file("prometheus-scrapeValues")]
 
   set = [
 	{
